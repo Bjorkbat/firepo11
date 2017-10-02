80fc396aa5aac72c35bd12d2244f3ef98ee2826a
Bjorkbat <bjorkbat@gmail.com>
2017-10-02 14:49:19 -0600

    Added logic/layout for defining a poll name

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,6 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
 ul {
   list-style-type: none;
   padding: 0;
@@ -42,4 +38,38 @@ li {
 a {
   color: #42b983;
 }
+
+form {
+
+  max-width: 900px;
+  margin: 0 auto;
+
+  input.form-control.underlined {
+    border: none;
+    border-bottom: 1px solid #2c3e50;
+    border-radius: 0;
+  }
+
+  input.big, input.medium {
+    text-align: center;
+    font-weight: bold;
+  }
+
+  input.big {
+    font-size: 32px;
+  }
+
+  input.medium {
+    font-size:18px;
+  }
+
+}
+
+section {
+  border: 2px dashed #FF2E00;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 25px auto;
+}
+
 </style>
--- src/components/CreatePoll.vue
+++ src/components/CreatePoll.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Create Poll</h1>
+  <div>
+
+    <h1>Create Poll</h1>
+
+    <hr />
+
+    <form v-on:submit.prevent="createPoll">
+
+      <section>
+        <div class="form-group">
+          <label class="h2">Poll Name</label>
+          <p class="lead">A good poll deserves a name.  Give it one here.</p>
+          <input type="text" class="form-control underlined big" v-model="name"
+            placeholder="Give your poll a fancy name or title here">
+        </div>
+      </section>
+
+    </form>
+
+  </div>
 </template>
+
+<script>
+export default {
+
+  data: function() {
+    return {
+      name: ''
+    }
+  }
+
+  methods: {
+
+    createPoll: function() {
+
+    }
+
+  }
+}
+</script>
